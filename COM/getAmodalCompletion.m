bf7310ecadde89b63620a496db7e8a59eb91687e
Linuxqw <weiq_cs@163.com>
2014-10-16 13:38:04 +0800

    last

--- COM/getAmodalCompletion.m
+++
@@ -1,8 +0,0 @@
-function [clusters, superpixels, thresh, ucmThresh] = getAmodalCompletion(imName)
-	paths = getPaths();
-	dt = load(fullfile(paths.amodalDir, strcat(imName, '.mat')));
-	clusters = dt.clusters;
-	thresh = dt.thresh;
-	ucmThresh = dt.ucmThresh;
-	superpixels = dt.superpixels;
-end
--- COM/getColorImage.m
+++
@@ -1,4 +0,0 @@
-function I = getColorImage(imName)
-	paths = getPaths();
-	I = imread(fullfile(paths.colorImageDir, strcat(imName, '.png')));
-end
